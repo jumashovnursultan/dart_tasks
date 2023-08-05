e4652c95e068ea027aaaed2969235fd30a05690d
nursultanjumashov <nursultanjumashovv@gmail.com>
2023-08-05 13:34:28 +0600

    map

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:dart_tasks/list.dart';
+import 'package:dart_tasks/map.dart';
 import 'package:dart_tasks/set.dart';
 
 void main() {
   list();
   set();
+  map();
 }
---
+++ lib/map.dart
@@ -0,0 +1,49 @@
+map() {
+  // var map = {1: "Tom", 2: "Bob", 3: "Sam"};
+  // var second = map[2]; // получим значение элемента с ключом 2
+  // print(second); // Bob
+
+  // map[1] = "Tomas"; // изменим значение элемента с ключом 1
+  // print(map);
+
+  // var map = {1: "Tom", 2: "Bob", 3: "Sam"};
+  // print("Length: ${map.length}"); // Length: 3
+  // print("Empty: ${map.isEmpty}"); // Empty: false
+  // print(map.entries); // (MapEntry(1: Tom), MapEntry(2: Bob), MapEntry(3: Sam))
+  // print(map.keys); // (1, 2, 3)
+  // print(map.values); // (Tom, Bob, Sam)
+
+  // var map = { 1: "Tom", 2: "Bob", 3: "Sam" };
+  // for(final element in map.entries){
+  //     print(element.key);     // получаем ключ элемента
+  //     print(element.value);     // получаем значение элемента
+  // }
+
+  // var map = {1: "Tom", 2: "Bob", 3: "Sam"};
+  // // перебираем ключи
+  // for (final key in map.keys) {
+  //   print(key);
+  // }
+  // // перебираем значения
+  // for (final value in map.values) {
+  //   print(value);
+  // }
+
+  // var map = {1: "Tom", 2: "Bob", 3: "Sam"};
+  // map.forEach((key, value) => print("$key - $value"));
+
+  // var women = {4: "Kate", 5: "Alice"};
+  // var map = {1: "Tom", 2: "Bob", 3: "Sam"};
+  // map.addAll(women); // добавляем словарь women
+  // print(map); // {1: Tom, 2: Bob, 3: Sam, 4: Kate, 5: Alice}
+
+  //   var map ={ 1: "Tom", 2: "Bob", 3: "Sam" };
+  // map.remove(2);   // удаляем элемент с ключом 2
+  // print(map);     // {1: Tom, 3: Sam}
+
+  // var map ={ 1: "Tom", 2: "Bob", 3: "Sam" };
+  // print(map.containsKey(2));          // true
+  // print(map.containsKey(4));          // false
+  // print(map.containsValue("Sam"));    // true
+  // print(map.containsValue("Tim"));    // false
+}
