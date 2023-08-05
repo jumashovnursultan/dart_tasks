f619540a56c2bc523a4fc90872dc4088332f2054
nursultanjumashov <nursultanjumashovv@gmail.com>
2023-08-05 13:16:22 +0600

    set

---
+++ lib/set.dart
@@ -0,0 +1,45 @@
+set() {
+  // var set = {"Tom", "Bob", "Sam"};
+  // for (var name in set) {
+  //   print(name);
+  // }
+
+  // var set = {"Tom", "Bob", "Sam"};
+  // set.forEach(print);
+
+  // var set = {"Tom", "Bob", "Sam", "Kate"};
+  // print(set.elementAt(0));
+
+  // var set = {"Tom", "Bob", "Sam"};
+  // set.add("Alice");
+  // print(set); // {Tom, Bob, Sam, Alice}
+
+  // var set = {"Tom", "Bob", "Sam"};
+  // set.addAll(["Alice", "Kate"]);
+  // print(set); // {Tom, Bob, Sam, Alice, Kate}
+
+  // var set = {"Tom", "Bob", "Sam"};
+  // print(set.contains("Tom")); //
+
+  // var set = {"Tom", "Bob", "Sam"};
+  // set.remove("Bob");
+  // print(set);    // {Tom, Sam}
+
+  // var set = {"Tom", "Alice", "Bob", "Sam", "Kate"};
+  // set.removeWhere((item) => item.length == 3); // если длина строки - 3 символа
+  // print(set); // {Alice, Kate}
+
+  // var set = {"Tom", "Alice", "Bob", "Sam", "Kate"};
+  // set.removeAll({"Alice", "Bob", "Alex"});
+  // print(set); // {Tom, Sam, Kate}
+
+  // var dart = {"Tom", "Alice", "Bob", "Mike"}; // изучают Dart
+  // var flutter = {"Tom", "Alice", "Sam", "Kate"}; // изучают Flutter
+  // var set = dart.intersection(flutter); // учат и dart, и flutter одновременно
+  // print(set); // {Tom, Alice}
+
+  //  var dart = {"Tom", "Alice", "Bob", "Mike"};     // изучают Dart
+  //   var flutter = {"Tom", "Alice","Sam", "Kate"};   // изучают Flutter
+  //   var set = dart.union(flutter);   // в обоих группах
+  //   print(set);   // {Tom, Alice, Bob, Mike, Sam, Kate}
+}
