b770391883e8d5f8b77a460f2320359c21767707
nursultanjumashov <nursultanjumashovv@gmail.com>
2023-08-06 12:35:02 +0600

    records

---
+++ lib/records.dart
@@ -0,0 +1,29 @@
+records() {
+  // var person = ("Tom", 38);
+  // print(person); // ("Tom", 38)
+
+  // (String, int) person = ('ABC', 2);
+  // print(person.$1);
+
+  // var person = (name:"Tom", age:38);
+  // print(person.name);   // Tom
+  // print(person.age);   // 38
+
+  // ({int age, String name}) person = (name:"Tom", age:38);
+  // print(person.name);   // Tom
+  // print(person.age);   // 38
+
+  // var person = defaultPerson();
+  // print(person); // ("Tom", 22)
+}
+
+// (String, int) defaultPerson() {
+//   return ("Tom", 22);
+// }
+
+// ({String name, int age}) defaultPerson2(){
+   
+//   return (name:"Tom", age:22);
+// }
+
+
