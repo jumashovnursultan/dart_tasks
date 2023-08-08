ba019c324fc09c2183bb6ec03864d923530ad676
nursultanjumashov <nursultanjumashovv@gmail.com>
2023-08-08 13:00:44 +0600

    try

---
+++ lib/try.dart
@@ -0,0 +1,14 @@
+functionTry() {
+  try {
+    int x = 9;
+    int y = 0;
+    int z = x ~/ y;
+
+    print(z);
+  } catch (e) {
+    print("Возникло исключение $e");
+  } finally {
+    print("Выполнение блока finally");
+  }
+  print("Завершение программы");
+}
