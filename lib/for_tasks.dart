1b2a6b08db08a2acebdd5c22378321afc464f24f
jumashovnursultan <nursultanjumashovv@gmail.com>
2023-09-21 19:38:43 +0600

    save

--- lib/for_tasks.dart
+++ lib/for_tasks.dart
@@ -1,10 +1,9 @@
 forTasks() {
-  // int sum = 0;
-  // for (int i = 1; i < 10; i++) {
-  //   sum += i;
-  // }
-  // print(sum);
-  //
+  int sum = 0;
+  for (int i = 1; i < 10; i++) {
+    sum += i;
+  }
+  print(sum);
 
   // int s = 19999;
   // int n = 5 * 12;
