04dc53bf4f21f529555e10139630b530d0f8154a
jumashovnursultan <nursultanjumashovv@gmail.com>
2023-10-19 21:52:34 +0600

    add class (Manufacturer, Product)

--- lib/class_tasks2.dart
+++ lib/class_tasks2.dart
@@ -1,2 +1,34 @@
+class Manufacturer {
+  String name;
+  String face;
+  List<String>? produce;
+
+  Manufacturer({
+    required this.name,
+    required this.face,
+    this.produce,
+  });
+}
+
+class Product {
+  String name;
+  int amount;
+  Manufacturer? manufacturer;
+  Document? document;
+
+  Product({
+    required this.name,
+    required this.amount,
+    this.manufacturer,
+    this.document,
+  });
+}
+
+class Document {
+  String dock;
+  Document({
+    required this.dock,
+  });
+}
+
 classTasks2() {}
-/////
