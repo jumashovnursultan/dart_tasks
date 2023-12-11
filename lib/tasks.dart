b8e38a9681c20b1f3f48a2a30bbcbb8e15f2649c
jumashovnursultan <nursultanjumashovv@gmail.com>
2023-12-11 15:35:55 +0600

    Fix twoSum function to return indexes of numbers that add up to target

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -1,11 +1,21 @@
 tasks() {
-  var target = 3;
-  var nums = [1, 2, 3, 4, 5];
+  final list = twoSum([2, 7, 11, 15], 9);
+  print(list);
+}
 
+List<int> twoSum(List<int> nums, int target) {
+  var indexes = <int>[];
   for (var i = 0; i < nums.length; i++) {
-    print(i);
-    // final result = nums[i] + nums[i++];
-    // print("${nums[i]}: $result");
+    for (var index = 0; index < nums.length; index++) {
+      if (i != index) {
+        final result = nums[i] + nums[index];
+        if (target == result) {
+          indexes.add(i);
+          indexes.add(index);
+          return indexes;
+        }
+      }
+    }
   }
-  // final result = nums[0] + nums[5];
+  return indexes;
 }
